9ac2f5a8b757918979718ee866d0378f9d88c9b1
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-23 15:43:35 +0700

    add portfolio and update home page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const StyledButton = styled.button`
   transition: all 0.3s ease-in-out;
 
   &:hover {
-    background-color: #000;
+    background-color: rgb(227, 226, 223);
     color: #fff;
   }
 `;
@@ -23,27 +23,22 @@ function Button({ children }) {
   return <StyledButton>{children}</StyledButton>;
 }
 
-function MyProfile( {name, age, children} ) {
-  return (
-    <div>
-      <h2>My name is {name}, and I'm {age} years old.</h2>
-      <p>{children}</p>
-    </div>
-  );
-}
-
 function Home() {
   return (
     <>
       <main>
-        <h2>Home</h2>
-        <p>Welcome to the homepage</p>
+        <div className="home-page">
+          
+            <div className="container">
+            <img src="https://picsum.photos/200/300" alt="random" />
+            <h1>Welcome to My Page!</h1>
+              <Button>
+                <Link to="/about">About</Link>
+              </Button>
+            </div>
+          
+        </div>
       </main>
-      <nav>
-        <Button>
-          <Link to="/about">About</Link>
-        </Button>
-      </nav>
     </>
   );
 }
@@ -52,15 +47,58 @@ function About() {
   return (
     <>
       <main>
-        <h2>About</h2>
-        <MyProfile name="John" age="30">
-          I'm a software developer.
-        </MyProfile>
-      </main>
-      <Button>
-          <Link to="/">Home</Link>
-        </Button>
-    </>
+      <div className="about-page">
+        <div className="container">
+            <div className="background"></div>
+            <div className="card">
+                <div className="card-background"></div>
+                <div className="profile-picture">
+                    <img src="/1468464029362.jpg" style={{ width: '31%', borderRadius: '20px' }} alt="Profile Picture" />
+                    <div className="flex-column">
+                        <h2>Shiggy Diggy</h2>
+                        <p>Web Developer</p>
+                    </div>
+                </div>
+                <div className="contact-info">
+                    <h2>Contacts</h2>
+                    <ul>
+                        <li>Phone: 123-456-7890</li>
+                        <li>Email: abc@abc.xyz</li>
+                        <li>
+                            <img src="/X_logo_2023.svg" style={{ width: '15px' }} alt="X Logo"></img>
+                                <a>: test123</a>
+                        </li>
+                        <li>
+                            <img src="/iconmonstr-linkedin-3.svg" style={{ width: '15px' }} alt="LinkedIn Logo"></img>
+                                <a>: test123</a>
+                        </li>
+                        <li>
+                            <img src="/GitHub_Invertocat_Logo.svg" style={{ width: '15px' }} alt="GitHub Logo"></img>
+                                <a>: test123</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="skills">
+                    <h2>Skills</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>PHP</li>
+                        <li>SQL</li>
+                    </ul>
+                    <h2>Links</h2>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </main>
+  </>
   );
 }
 
@@ -68,11 +106,10 @@ function App() {
   return (
     <div className="App">
       <header>
-      <h1>Welcome to React Router</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </header>
     </div>
   );
