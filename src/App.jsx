1785b2ad90c2702fe7b99c7a429878e9b9c3e4e3
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-23 14:01:44 +0700

    Add styling and routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './App.css';
 
-function MyProfile( {name, age, children} ) {
+// function MyProfile( {name, age, children} ) {
+//   return (
+//     <div>
+//       <h2>My name is {name}, and I'm {age} years old.</h2>
+//       <p>{children}</p>
+//     </div>
+//   );
+// }
+
+function Home() {
   return (
-    <div>
-      <h2>My name is {name}, and I'm {age} years old.</h2>
-      <p>{children}</p>
-    </div>
+    <>
+      <main>
+        <h2>Home</h2>
+        <p>Welcome to the homepage</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>About</h2>
+        <p>Welcome to the about page</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MyProfile name="John" age="20">I'm a student.</MyProfile>
+      <header>
+      <h1>Welcome to React Router</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       </header>
     </div>
   );
